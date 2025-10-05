1d0933416de1207a8bb8a6be64cc83574a303e7a
Pallav Kumar <furinehigh@outlook.com>
2025-10-05 09:43:55 +0530

    added user and community as new psql schema

---
+++ app/api/community/add/route.ts
@@ -0,0 +1,12 @@
+import { NextRequest, NextResponse } from "next/server"
+
+export const POST = async (req: NextRequest) => {
+  try {
+    const body = await req.json()
+
+    return NextResponse.json({ message: "Request received", data: body }, { status: 200 })
+  } catch (err) {
+    console.error("POST error:", err)
+    return NextResponse.json({ error: "Invalid request" }, { status: 400 })
+  }
+}
---
+++ app/api/images/upload/route.ts
@@ -0,0 +1,14 @@
+import { NextRequest, NextResponse } from "next/server"
+
+export const POST = async (req: NextRequest) => {
+  try {
+    const body = await req.json()
+
+    const {image, communityId, userId, } = body
+
+    return NextResponse.json({ message: "Request received", data: body }, { status: 200 })
+  } catch (err) {
+    console.error("POST error:", err)
+    return NextResponse.json({ error: "Invalid request" }, { status: 400 })
+  }
+}
