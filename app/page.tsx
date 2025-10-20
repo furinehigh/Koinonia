fe5ee512c3d375cc4a1a788a5a81f80f19a300a8
Pallav kumar <furinehigh@outlook.com>
2025-10-20 09:53:11 +0000

    fixed map fun not exist error

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ import Link from "next/link";
 export default async function Home() {
   const posts = await recentPosts()
   const session = await getServerSession(authOptions)
-  let activities;
+  let activities = [];
   if (session && session.user) {
     activities = await getUserActivities(session?.user.id)
   }
