9c1c4ac02dd23edf91c3c146cc2ad8900a54adae
Pallav kumar <furinehigh@outlook.com>
2025-11-02 17:54:22 +0000

    unapproved post r now only vissible to mods and the author

--- app/n/[slug]/post/[postId]/page.tsx
+++ app/n/[slug]/post/[postId]/page.tsx
@@ -4,13 +4,16 @@ import NotFound from './not-found'
 import { Post } from '@/types'
 import PostPage from '@/components/post/PostPage'
 import { getAllPostComments, getAllUserComments } from '@/lib/data/comments'
+import { getServerSession } from 'next-auth'
+import { authOptions } from '@/lib/auth'
 
 async function page({params}: {
   params: {postId: string}
 }) {
   const postId = await params.postId
+  const session = await getServerSession(authOptions)
 
-  const post = await getPost(postId) as Post
+  const post = await getPost(postId, session?.user.id) as Post
   if (!post) return NotFound();
   const comments = await getAllPostComments(postId)
   return (
