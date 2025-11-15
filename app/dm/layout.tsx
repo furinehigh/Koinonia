f8c28ece81d528996f524a2ff818654775e645d0
Pallav kumar <furinehigh@outlook.com>
2025-11-15 18:18:42 +0000

    made few chat's UI updates :)

--- app/dm/layout.tsx
+++ app/dm/layout.tsx
@@ -1,9 +1,18 @@
 import Sidebar from '@/components/DM/Sidebar'
+import { authOptions } from '@/lib/auth'
+import { getServerSession } from 'next-auth'
+import { redirect } from 'next/navigation'
 import React from 'react'
 
-function layout({ children }: {
+async function layout({ children }: {
     children: React.ReactNode
 }) {
+
+    const session = await getServerSession(authOptions)
+    if (!session?.user || !session.user.id) {
+        redirect('/signin')
+    }
+
     return (
         <div className='flex'>
             <Sidebar recentDMs={[
@@ -13,14 +22,14 @@ function layout({ children }: {
                         avatarUrl: '',
                         name: 'Test User'
                     }
-                },{
+                }, {
                     id: 'sdfs',
                     reciever: {
                         avatarUrl: '',
                         name: 'Test User 2'
                     }
                 },
-                ]} />
+            ]} />
             <div className='m-3 w-full'>
                 {children}
             </div>
--- components/DM/ChatUI.tsx
+++ components/DM/ChatUI.tsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import { Input } from '../ui/input'
+import { Button } from '../ui/button'
 
 function ChatUI() {
   return (
     <div className='relative h-full w-full'>
-      <div className='fixed bottom-2 w-fit mr-5 z-[56]'>
-        <Input placeholder='Type a message...' className='p-5 rounded-full ' />
+      <div className='mt-5 flex flex-col gap-2 w-full'>
+        <p className='bg-gray-100 rounded-full w-fit px-2 py-1 text-sm'>Hello</p>
+        <p className='bg-gray-800 text-white ml-auto rounded-full w-fit px-2 py-1 text-sm'>Hi</p>
+      </div>
+      <div className='bg-white fixed bottom-0 w-full pb-3 pt-1'>
+        <Input placeholder='Type a message...' className='p-5 rounded-full w-[70vw] z-[56]' />
       </div>
     </div>
   )
